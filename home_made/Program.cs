0c3991fb1b0eaffeba440497963f7835ed251f9a
BARBAROS İLHAN <barbarosthk@gmail.com>
2025-05-13 19:09:09 +0300

    ozet

---
+++ home_made/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace home_made;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Merhaba! Ben Barbaros İlhan.");
+        Console.WriteLine("Yazılım dünyasını yeni keşfediyorum .");
+        Console.WriteLine("Bu yüzden en iyi keşfin yapılabileceği adrese geldim . Patika.dev");
+
+
+    }
+}
+
+
